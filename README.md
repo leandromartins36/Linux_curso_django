a1db2b29560a5947608be95e8a29bbef4650a65f
leandro de menezes martins <leoinfosiste@yahoo.com.br>
2021-08-14 17:30:02 -0300

    configurando o postgres como db principal

--- README.md
+++ README.md
@@ -4,6 +4,6 @@ Código desenvolvido no módulo de Django do [Site Python Pro](www.python.pro.br
 Aplicação disponível em https://linuxmintcursodjango130821.herokuapp.com/
 
 #testando os imports do travis
-[![Build Status](https://travis-ci.com/leandromartins36/Linux_curso_django.svg?branch=main)](https://travis-ci.com/leandromartins36/Linux_curso_django)
 #criar variavel de alias para mng 
 #alias mng='python $VIRTUAL_ENV/../manage.py'
+[![Build Status](https://app.travis-ci.com/leandromartins36/Linux_curso_django.svg?branch=main)](https://app.travis-ci.com/leandromartins36/Linux_curso_django)
--- pypro/settings.py
+++ pypro/settings.py
@@ -9,8 +9,11 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-from decouple import config
+import os
+from functools import partial
+from decouple import config, Csv
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,12 +22,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ax$9jy^q=j)svu5ea_mf^j%th7lebj1(kdn@psqn-e*^=#defl'
+# SECRET_KEY = 'django-insecure-ax$9jy^q=j)svu5ea_mf^j%th7lebj1(kdn@psqn-e*^=#defl'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
+
+# case acontecer erro colocar esta configuração: DEBUG = config('DEBUG', default=False, cast=bool)
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = []
+# ALLOWED_HOSTS = config('*', 'ALLOWED_HOSTS', cast=Csv)
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
@@ -72,11 +79,12 @@ WSGI_APPLICATION = 'pypro.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+default_db_url = 'sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')
+
+parse_database = partial(dj_database_url.parse, conn_max_age=600)
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': config('DATABASE_URL', default=default_db_url, cast=parse_database)
 }
 
 # Password validation
