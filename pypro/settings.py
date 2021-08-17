324ad75f6cd6bf825ed75ae8f2a5fa6ead5be3f9
leandro de menezes martins <leoinfosiste@yahoo.com.br>
2021-08-17 09:01:08 -0300

    INSTALANDO O COLLECTFAST

--- pypro/settings.py
+++ pypro/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'collectfast',
     'django.contrib.staticfiles',
     'pypro.base',
 
@@ -127,6 +128,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles')
 
+COLLECTFAST_ENABLED = False
+
 AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
 # STORAGE CONFIGURATION IN S3 AWS
 if AWS_ACCESS_KEY_ID:
@@ -136,6 +139,9 @@ if AWS_ACCESS_KEY_ID:
     AWS_PRELOAD_METADATA = True
     AWS_AUTO_CREATE_BUCKET = False
     AWS_QUERYSTRING_AUTH = True
+
+    COLLECTFAST_ENABLED = True
+
     AWS_S3_CUSTOM_DOMAIN = None
     COLLECTFAST_STRATEGY = "collectfast.strategies.boto3.Boto3Strategy"
     COLLECTFAST_ENABLED = True
