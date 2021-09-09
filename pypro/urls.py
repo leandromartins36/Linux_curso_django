5bfae86022d22f9161c21fff99290df764eaaa3e
leandro de menezes martins <leoinfosiste@yahoo.com.br>
2021-09-09 11:35:49 -0300

    inclusão da pagina do video de motivação e criação do app video

--- pypro/urls.py
+++ pypro/urls.py
@@ -20,6 +20,8 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('pypro.base.urls')),
+    path('aperitivos/', include('pypro.aperitivos.urls')),
+
 
 ]
 
